bbf69432249997a95cae93877d0c8570eab698d9
Ghozti <ybalderas1005@gmail.com>
2021-01-06 16:13:17 -0500

    set fixed values for all 3 ranks

--- src/ghozti/pokemon/game/constants/Constants.java
+++ src/ghozti/pokemon/game/constants/Constants.java
@@ -7,39 +7,31 @@ public class Constants {
     * TODO set the rates for rank 1 & 2
      */
 
-    public static class UserRank1rates {
+    public static final double pokemonEv1Rate = .65;
+    public static final double pokemonEv2Rate = .30;
+    public static final double pokemonEv3Rate = .05;
+
+    public static class UserRank1rates extends Constants{
         public static final double pbRate = .25;
         public static double gbRate = .50;
         public static double ubRate = .75;
         public static double mbRate = 1.0;
         public static double qbRate = .35;
-
-        public static final double pokemonEv1Rate = .65;
-        public static final double pokemonEv2Rate = .30;
-        public static final double pokemonEv3Rate = .05;
     }
 
-    public static class UserRank2rates {
-        public static final double pbRate = .25;
-        public static double gbRate = .50;
-        public static double ubRate = .75;
+    public static class UserRank2rates extends Constants{
+        public static final double pbRate = .20;
+        public static double gbRate = .46;
+        public static double ubRate = .73;
         public static double mbRate = 1.0;
-        public static double qbRate = .35;
-
-        public static final double pokemonEv1Rate = .65;
-        public static final double pokemonEv2Rate = .30;
-        public static final double pokemonEv3Rate = .05;
+        public static double qbRate = .32;
     }
 
-    public static class UserRank3rates {
-        public static final double pbRate = .25;
-        public static double gbRate = .50;
-        public static double ubRate = .75;
+    public static class UserRank3rates extends Constants{
+        public static final double pbRate = .15;
+        public static double gbRate = .40;
+        public static double ubRate = .68;
         public static double mbRate = 1.0;
-        public static double qbRate = .35;
-
-        public static final double pokemonEv1Rate = .65;
-        public static final double pokemonEv2Rate = .30;
-        public static final double pokemonEv3Rate = .05;
+        public static double qbRate = .28;
     }
 }
