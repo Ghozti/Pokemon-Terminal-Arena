dd2a4080ad8298f5090f7d5e24bb189e81daaaa3
Ghozti <ybalderas1005@gmail.com>
2021-01-09 20:26:20 -0500

    made the shop functional. However this is not the final product as it has the same issue the catch pokemon method had. So i will work on making it more efficient and better. Also i need to add an exit function because players can be a pain.

--- src/ghozti/pokemon/game/environment/Shop.java
+++ src/ghozti/pokemon/game/environment/Shop.java
@@ -1,5 +1,8 @@
 package ghozti.pokemon.game.environment;
 
+import ghozti.pokemon.game.user.User;
+import ghozti.pokemon.game.user.UserMethods;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,6 +12,7 @@ public class Shop {
         Scanner scanner = new Scanner(System.in);
         int choice;
         System.out.println("Welcome to the PokeCenter!, what would you like to buy?");
+        System.out.println("***OPTIONS***");
         String[] list = new String[6];
 
         list[0] = "[1] Pokeball ($10 per ball)";
@@ -16,10 +20,12 @@ public class Shop {
         list[2] = "[3] Quick Ball ($15 per ball)";
         list[3] = "[4] Ultra Ball ($45 per ball)";
         list[4] = "[5] Master Ball ($550 per ball)";
-        list[5] = "[6] Health Potion ($ per potion)";
+        list[5] = "[6] Health Potion ($10 per potion)";
         for (String i : list) {
             System.out.println(i);
         }
+        System.out.println("***INVENTORY***");
+        UserMethods.printInventory();
         try {
             choice = scanner.nextInt();
             if(choice == 0){
@@ -37,13 +43,66 @@ public class Shop {
         return 0;
     }
 
-    private static void messages(int ind){
-        String[] messages = new String[2];
-        messages[0] = "Great!, how many do you want?";
-        messages[1] = "Not enough cash!";
-    }
+    public static void shop(){
+        Scanner scanner = new Scanner(System.in);
+        int choice = shopMenu();
+        int amount;
 
-    private static void buyItem(int item, String itemName){
-
+        switch (choice){
+            case 1:
+                System.out.println("Great! How Many Do You Want?");
+                amount = scanner.nextInt();
+                if(amount * 10 > User.money){
+                    System.out.println("***YOU DO NOT HAVE ENOUGH!!***");
+                }else{
+                    User.money -= (amount * 10);
+                    System.out.println("***ADDED " + amount + " POKEBALL(S) TO YOUR INVENTORY!***");
+                }
+            case 2:
+                System.out.println("Great! How Many Do You Want?");
+                amount = scanner.nextInt();
+                if(amount * 20 > User.money){
+                    System.out.println("***YOU DO NOT HAVE ENOUGH!!***");
+                }else{
+                    User.money -= (amount * 20);
+                    System.out.println("***ADDED " + amount + " POKEBALL(S) TO YOUR INVENTORY!***");
+                }
+            case 3:
+                System.out.println("Great! How Many Do You Want?");
+                amount = scanner.nextInt();
+                if(amount * 15 > User.money){
+                    System.out.println("***YOU DO NOT HAVE ENOUGH!!***");
+                }else{
+                    User.money -= (amount * 15);
+                    System.out.println("***ADDED " + amount + " POKEBALL(S) TO YOUR INVENTORY!***");
+                }
+            case 4:
+                System.out.println("Great! How Many Do You Want?");
+                amount = scanner.nextInt();
+                if(amount * 45 > User.money){
+                    System.out.println("***YOU DO NOT HAVE ENOUGH!!***");
+                }else{
+                    User.money -= (amount * 45);
+                    System.out.println("***ADDED " + amount + " POKEBALL(S) TO YOUR INVENTORY!***");
+                }
+            case 5:
+                System.out.println("Great! How Many Do You Want?");
+                amount = scanner.nextInt();
+                if(amount * 550 > User.money){
+                    System.out.println("***YOU DO NOT HAVE ENOUGH!!***");
+                }else{
+                    User.money -= (amount * 550);
+                    System.out.println("***ADDED " + amount + " POKEBALL(S) TO YOUR INVENTORY!***");
+                }
+            case 6:
+                System.out.println("Great! How Many Do You Want?");
+                amount = scanner.nextInt();
+                if(amount * 10 > User.money){
+                    System.out.println("***YOU DO NOT HAVE ENOUGH!!***");
+                }else{
+                    User.money -= (amount * 10);
+                    System.out.println("***ADDED " + amount + " POKEBALL(S) TO YOUR INVENTORY!***");
+                }
+        }
     }
 }
