32641f311e81c27f19336d042055f840d4897c80
Ghozti <ybalderas1005@gmail.com>
2021-01-11 16:10:00 -0500

    finished the options menu method. All i need to work on now is the actual methods

--- src/ghozti/pokemon/game/environment/GameStructure.java
+++ src/ghozti/pokemon/game/environment/GameStructure.java
@@ -1,8 +1,30 @@
 package ghozti.pokemon.game.environment;
 
+import java.util.Scanner;
+
 public class GameStructure {
     public static int optionMenu(){
-        return 0;
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        String[] options = new String[5];
+        options[0] = "[1] Check Inventory";
+        options[1] = "[2] Go to Nearest Pokemon Center";
+        options[2] = "[3] Go to wild grass";
+        options[3] = "[4] Check pokemon(s)";
+        options[4] = "[5] Look for a battle";
+
+        for (String i : options){
+            System.out.println(i);
+        }
+        System.out.println("Enter your choice: ");
+        choice = scanner.nextInt();
+
+        if(choice < 1 || choice > 6){
+            System.out.println("***THIS IS NOT A VALID INPUT PLEASE ENTER A VALID INPUT***");
+            optionMenu();
+        }
+        return choice;
     }
 
     public static void option1(){
