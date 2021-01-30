94132d65324c4f0af9bbcd2e9b3ccf4d7cc12c10
Ghozti <ybalderas1005@gmail.com>
2021-01-30 16:22:11 -0500

    finished a prototype player battle method. TEST IT

--- src/ghozti/pokemon/game/battles/PlayerBattles.java
+++ src/ghozti/pokemon/game/battles/PlayerBattles.java
@@ -1,6 +1,8 @@
 package ghozti.pokemon.game.battles;
 
 import ghozti.pokemon.game.pokemon.Pokemon;
+import ghozti.pokemon.game.pokemon.PokemonUtils;
+import ghozti.pokemon.game.user.UserMethods;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -36,7 +38,8 @@ public class PlayerBattles {
     }
 
     public static void playerBattle(boolean ranked){
-
+        int pokeCount = 0;
+        //this variable will be used to assert the ranked battles only call 3 pokemon.
         Pokemon enemyPokemon = BattleUtils.getPokemon();
         //creates the wild pokemon obj
 
@@ -48,6 +51,7 @@ public class PlayerBattles {
         if (getUserChoice() == 1){
             uPokemon = BattleUtils.getUPokemon();
             //will get the user's pokemon object
+            uPokemon.printStats();
 
             while (uPokemon.HP > 0 || enemyPokemon.HP > 0){
                 System.out.println("Your move!: ");
@@ -56,12 +60,33 @@ public class PlayerBattles {
 
                 if (enemyPokemon.HP <= 0){
                     System.out.println("***YOU DEFEATED THE " + enemyPokemon.name.toUpperCase() + "***");
-                    //TODO make it so normal battles call 2 to 3 pokemon and ranked call 5
+                    //TODO make it so normal battles call 1 pokemon and ranked call 3
+                    if (pokeCount < 4){
+                        enemyPokemon = BattleUtils.getPokemon();
+                        pokeCount++;
+                        System.out.println("Your opponent has brought a new pokemon!");
+                        System.out.println("name: " + enemyPokemon.name);
+                        uPokemon.printStats();
+                    }
                 }
 
                 System.out.println("Your opponent's health: " + enemyPokemon.HP);
 
                 uPokemon.HP -= enemyPokemon.wildAttack();//it's wild but the function stays the same in terms of calling a radom move
+
+                if(uPokemon.HP <= 0){//if the user's pokemon health is less than or equal to 0
+                    System.out.println("***THE POKEMON DEFEATED YOUR POKEMON***");
+                    int defeatChoice = BattleUtils.defeatMenu();//will call the method to ask the user if they wish to call another pokemon or to flee
+
+                    if (defeatChoice == 1){//if the user chooses to call another pokemon
+                        uPokemon = BattleUtils.getUPokemon();//will replace the last user pokemon with a new object and continue the battle.
+                    }else if (defeatChoice == 2){//if the user admitted defeat
+                        System.out.println("***YOU ADMITTED DEFEAT***");
+                        UserMethods.addXp(2);
+                        PokemonUtils.addXp(5,uPokemon);
+                        return;
+                    }
+                }
             }
         }else{
             return;
