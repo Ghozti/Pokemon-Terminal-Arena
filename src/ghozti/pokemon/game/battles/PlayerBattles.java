9b972a3a551ba965e9570afb288baf864e9b85f6
Ghozti <ybalderas1005@gmail.com>
2021-01-30 15:15:04 -0500

    begun to add to the playerBattles

--- src/ghozti/pokemon/game/battles/PlayerBattles.java
+++ src/ghozti/pokemon/game/battles/PlayerBattles.java
@@ -37,10 +37,35 @@ public class PlayerBattles {
 
     public static void playerBattle(boolean ranked){
 
-        Pokemon wildPokemon = BattleUtils.getPokemon();
+        Pokemon enemyPokemon = BattleUtils.getPokemon();
         //creates the wild pokemon obj
 
+        Pokemon uPokemon;
+        //will represent the user's pokemon
+
         encounterMenu();
+
+        if (getUserChoice() == 1){
+            uPokemon = BattleUtils.getUPokemon();
+            //will get the user's pokemon object
+
+            while (uPokemon.HP > 0 || enemyPokemon.HP > 0){
+                System.out.println("Your move!: ");
+                enemyPokemon.HP -= uPokemon.getMove(enemyPokemon,false);
+                //wil get the move damage. The bool is set to false since the user cannot catch this pokemon
+
+                if (enemyPokemon.HP <= 0){
+                    System.out.println("***YOU DEFEATED THE " + enemyPokemon.name.toUpperCase() + "***");
+                    //TODO make it so normal battles call 2 to 3 pokemon and ranked call 5
+                }
+
+                System.out.println("Your opponent's health: " + enemyPokemon.HP);
+
+                uPokemon.HP -= enemyPokemon.wildAttack();//it's wild but the function stays the same in terms of calling a radom move
+            }
+        }else{
+            return;
+        }
         //calls the encounter menu and prints
     }
 }
