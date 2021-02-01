024a118d8648c2157de8701afab399793668f9f9
Ghozti <ybalderas1005@gmail.com>
2021-02-01 18:20:31 -0500

    set codes

--- src/ghozti/pokemon/game/codes/Codes.java
+++ src/ghozti/pokemon/game/codes/Codes.java
@@ -1,4 +1,37 @@
 package ghozti.pokemon.game.codes;
 
+import java.util.ArrayList;
+
 public class Codes {
+
+    public Codes(){
+        setCodes();
+    }//sets the codes automatically
+
+    public ArrayList<String> codes = new ArrayList<>(23);
+
+    private void setCodes(){
+        codes.add("92319");
+        codes.add("max");
+        codes.add("animal crossings");
+        codes.add("3080");
+        codes.add("3070");
+        codes.add("java");
+        codes.add("333");
+        codes.add("8bit");
+        codes.add("16 months");
+        codes.add("64bit");
+        codes.add("linux");
+        codes.add("raspberrypi");
+        codes.add("windows");
+        codes.add("samsung");
+        codes.add("gtaV");
+        codes.add("5900x");
+        codes.add("amd");
+        codes.add("nvidia");
+        codes.add("clones");
+        codes.add("order66");
+        codes.add("code");
+        codes.add("homer simpson");
+    }
 }
