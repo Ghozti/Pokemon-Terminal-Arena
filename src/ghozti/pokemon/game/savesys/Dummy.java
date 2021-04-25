d81d6225562ce707f88ae22c82993b0873afb1eb
Ghozti <ybalderas1005@gmail.com>
2021-04-25 11:56:45 -0400

    - added the Dummy class (for notes and reference)

---
+++ src/ghozti/pokemon/game/savesys/Dummy.java
@@ -0,0 +1,15 @@
+package ghozti.pokemon.game.savesys;
+
+public class Dummy {
+    /**
+     * to save:
+     * make a program that will write a bunch of text files containing user info such as:
+     * - money
+     * - balls
+     * - rank
+     * - ETC
+     * ***SAME APPLIES FOR EACH POKEMON IN THE USER***
+     * then make the program read those files and grant the user their items
+     */
+    //TODO delete this class when done
+}
